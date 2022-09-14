eeebc949df5edf4aa0d7a6682bae51b58cca0351
Luis Cataldo <luigi33579@gmail.com>
2022-09-14 18:44:03 -0300

    Update About.jsx

--- portfolio/src/componentes/About/About.jsx
+++ portfolio/src/componentes/About/About.jsx
@@ -19,10 +19,10 @@ export default function About({ setTheme, theme }) {
             <h1 fontFamily="'Nuosu SIL', serif">Luis Cataldo</h1>
             <h3 fontFamily="'Nuosu SIL', serif">Full Stack Developer</h3>
             <p>
-              Soy una persona proactiva, organizada y responsable, con buenas relaciones interpersonales.
-              Siempre tengo la mejor disposición para la realización de mis labores. <br />
-              Busco un puesto de trabajo desafiante donde pueda aprender nuevas tecnologías y hacer crecer mis conocimientos profesionales.<br />
-              Mi proposito es poder dominar el desarrollo web con buenas practicas y a la perfeccion para luego avanzar a otro sector como el desarrollo de aplicaciones o de video juegos. En definitiva poder desarrollarme profesionalmente en todo lo que es el desarrollo.
+              Soy una persona responsable, comprometida y organizada; con gran capacidad de trabajar en equipo y dar mi 101% demostrando mi potencial en las metas propuestas.
+              Me interesa continuar profundizando mis conocimientos, aprendizaje y desarrollo en diferentes áreas, 
+              relacionadas a mi carrera profesional y otras diferentes capaces de aumentar mi visión y horizontes, 
+                para nutrir mi desarrollo personal como miembro activo de la comunidad.
             </p>
             <Center> <div className="project--button">
               <a href={resume} target="_blank" className="button button--primary">Ver Resumen</a>
@@ -33,4 +33,8 @@ export default function About({ setTheme, theme }) {
     </div>
   )
 }
+
+//Soy una persona proactiva, organizada y responsable, con buenas relaciones interpersonales. Siempre tengo la mejor disposición para la realización de mis labores.
+//Busco un puesto de trabajo desafiante donde pueda aprender nuevas tecnologías y hacer crecer mis conocimientos profesionales.
+//Mi proposito es poder dominar el desarrollo web con buenas practicas y a la perfeccion para luego avanzar a otro sector como el desarrollo de aplicaciones o de video juegos. En definitiva poder desarrollarme profesionalmente en todo lo que es el desarrollo.
 
